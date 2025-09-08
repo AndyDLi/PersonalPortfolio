7ad0ef38aca1c6fb7a282f44bed0da0bcbe6d7c8
Andy Li <andy.dang.li@gmail.com>
2025-09-08 09:24:45 -0400

    Added a Skills Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar.jsx';
-import { Banner } from './components/Banner.jsx'; 
+import { Banner } from './components/Banner.jsx';
+import { Skills } from './components/Skills.jsx'; 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
