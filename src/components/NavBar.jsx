a52175612dca516127431ca23d67f03a4795f131
Andy Li <andy.dang.li@gmail.com>
2025-09-14 15:11:38 -0400

    Updated NavBar Display when Screen Shrinks

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,6 +11,7 @@ import mailIcon from '../assets/images/mail.png';
 
 export const NavBar = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [expanded, setExpanded] = useState(false);
     const location = useLocation();
 
     useEffect(() => {
@@ -36,47 +37,76 @@ export const NavBar = () => {
         }
     };
 
+    const handleNavClick = () => {
+        setExpanded(false);
+    };
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
+    // Close Menu When Screen Size Changes to Desktop
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 991.98) {
+                setExpanded(false);
+            }
+        };
+        
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    <img src={logo} alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/" className={getActiveLink() === 'home' ? "active navbar-link" : "navbar-link"}>
-                            <img src={homeIcon} alt="Home Icon" />
-                            <p>Home</p>
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/skills" className={getActiveLink() === 'skills' ? "active navbar-link" : "navbar-link"}>
-                            <img src={personIcon} alt="Skills Icon" />
-                            <p>Skills</p>
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/projects" className={getActiveLink() === 'projects' ? "active navbar-link" : "navbar-link"}>
-                            <img src={projectsIcon} alt="Projects Icon" />
-                            <p>Projects</p>
-                        </Nav.Link>
-                        <Nav.Link as={Link} to="/contact" className={getActiveLink() === 'contact' ? "active navbar-link" : "navbar-link"}>
-                            <img src={mailIcon} alt="Contact Icon" />
-                            <p>Contact</p>
-                        </Nav.Link>
-                    </Nav>
+        <>
+            <div className={`mobile-menu-overlay ${expanded ? 'show' : ''}`} onClick={handleNavClick}></div>
+            
+            <Navbar expand="lg" expanded={expanded} className={scrolled ? "scrolled" : ""}>
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img src={logo} alt="Logo" />
+                    </Navbar.Brand>
 
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/andyli8/" target="_blank"><img src={linkedInIcon} alt="" /></a>
-                            <a href="https://github.com/AndyDLi" target='_blank'><img src={gitHubIcon} alt="" /></a>
-                        </div>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle}>
+                        <span className="navbar-toggler-icon"></span>
+                    </Navbar.Toggle>
 
-                        <button className="vvd">
-                            <a href="/contact"><span>Contact Me</span></a>
-                        </button>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/" className={getActiveLink() === 'home' ? "active navbar-link" : "navbar-link"} onClick={handleNavClick}>
+                                <img src={homeIcon} alt="Home Icon" />
+                                <p>Home</p>
+                            </Nav.Link>
+
+                            <Nav.Link as={Link} to="/skills" className={getActiveLink() === 'skills' ? "active navbar-link" : "navbar-link"} onClick={handleNavClick}>
+                                <img src={personIcon} alt="Skills Icon" />
+                                <p>Skills</p>
+                            </Nav.Link>
+
+                            <Nav.Link as={Link} to="/projects" className={getActiveLink() === 'projects' ? "active navbar-link" : "navbar-link"} onClick={handleNavClick}>
+                                <img src={projectsIcon} alt="Projects Icon" />
+                                <p>Projects</p>
+                            </Nav.Link>
+                            
+                            <Nav.Link as={Link} to="/contact" className={getActiveLink() === 'contact' ? "active navbar-link" : "navbar-link"} onClick={handleNavClick}>
+                                <img src={mailIcon} alt="Contact Icon" />
+                                <p>Contact</p>
+                            </Nav.Link>
+                        </Nav>
+
+                        <span className="navbar-text">
+                            <div className="social-icon">
+                                <a href="https://www.linkedin.com/in/andyli8/" target="_blank"><img src={linkedInIcon} alt="" /></a>
+                                <a href="https://github.com/AndyDLi" target='_blank'><img src={gitHubIcon} alt="" /></a>
+                            </div>
+
+                            <button className="vvd">
+                                <a href="/contact"><span>Contact Me</span></a>
+                            </button>
+                        </span>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 }
